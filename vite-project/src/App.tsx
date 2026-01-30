7113664ae85ab46eda5b33b668368664507e9caf
mowei <mowei@cityos.com>
2026-01-30 14:32:11 +0800

    feat: react 父子组件通信：react18-19不同定义形式

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,11 +2,25 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Card from "./components/Card";
 
 function App() {
   return (
     <>
-      <button onClick={() => window.onShow()}>全局弹窗</button>
+      {/* 可以传递 */}
+      <Card
+        title={"我喜欢男的"}
+        el={<div>扩展内容</div>}
+        isFlag={true}
+        empty={null}
+        arr={[1, 2]}
+        fn={() => console.log("函数")}
+        unde={undefined}
+      >
+        <div>内容区域</div>
+      </Card>
+      <Card title={"我喜欢女的"} />
+      {/* <button onClick={() => window.onShow()}>全局弹窗</button> */}
     </>
   );
 }
---
+++ vite-project/src/components/Card/index.tsx
@@ -0,0 +1,34 @@
+/*
+ * react 父子组件通信：react18-19
+ * 父子组件通信：子组件通过props接收
+ * props 属性可接受内容：1. 字符串 2. 数字 3. 布尔值 4. 对象 5. 数组 6. 函数 7. 组件 8. JSX元素（其中为一项时是对象形式、多项则为数组形式接收）
+ */
+import React from "react";
+import "./index.less";
+// 版本一
+interface Props {
+  title: string;
+  children?: React.ReactNode;
+  el?: React.ReactNode;
+}
+// export default function Card(props: Props | any) {
+//   console.log("props", props);
+//   return (
+//     <div className="card">
+//       <h2>{props.title}</h2>
+//       {props.children}
+//     </div>
+//   );
+// }
+
+// 版本二
+const Card: React.FC<Props> = props => {
+  return (
+    <div className="card">
+      <h2>{props.title}</h2>
+      {props.children}
+    </div>
+  );
+};
+
+export default Card;
