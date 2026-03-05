a16c90b16668dcfaf30dc8c8481a4ad9197cb8d5
mowei <mowei@cityos.com>
2026-03-05 11:01:47 +0800

    fix: 同上一提交

--- vite-project/src/components/useSyncExternalStore案例/useHistory.tsx
+++ vite-project/src/components/useSyncExternalStore案例/useHistory.tsx
@@ -1,19 +1,39 @@
-import { useEffect, useLayoutEffect, useReducer, useState } from "react";
-import "./App.css";
-/*
- * 使用 useSyncExternalStore
- */
-import { useHistory } from "./hooks/useHistory";
+import { useSyncExternalStore } from "react";
 
-function App() {
-  const [url, push, replace] = useHistory();
-  return (
-    <div id="container">
-      <p>地址：{url}</p>
-      <button onClick={() => push("/x")}>push</button>
-      <button onClick={() => replace("/y")}>replace</button>
-    </div>
-  );
-}
+/* 利用useSyncExternalStore 获取history内容与订阅变化 */
+export const useHistory = () => {
+  const subscribe = (callback: () => void) => {
+    // 订阅浏览器api 监听history变化
+    // vue 中有三种路由模式 ssr （服务端渲染） 浏览器 hash history
+    // hash模式 的底层是监听 hashchange 事件
+    // history模式 的底层是监听 popstate 事件
+    window.addEventListener("popstate", callback);
+    window.addEventListener("hashchange", callback);
+    return () => {
+      // 取消订阅
+      window.removeEventListener("popstate", callback);
+      window.removeEventListener("hashchange", callback);
+    };
+  };
+  // 获取当前浏览器地址
+  const getSnapshot = () => {
+    return window.location.href;
+  };
 
-export default App;
+  const url = useSyncExternalStore(subscribe, getSnapshot);
+
+  // popstate 只能监听浏览器的前进和后退按钮 无法监听 pushState 和 replaceState
+  // history push操作
+  const push = (url: string) => {
+    window.history.pushState(null, "", url);
+    // 手动触发 popstate 事件，以便其他监听器能够接收到更新
+    window.dispatchEvent(new PopStateEvent("popstate"));
+  };
+  // history replace操作
+  const replace = (url: string) => {
+    window.history.replaceState(null, "", url);
+    // 手动触发 popstate 事件，以便其他监听器能够接收到更新
+    window.dispatchEvent(new PopStateEvent("popstate"));
+  };
+  return [url, push, replace] as const;
+};
